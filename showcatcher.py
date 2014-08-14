23b53b61ef4869626d5832899120400e9e9ff90d
archangelic <michaelhancock89@gmail.com>
2014-08-14 12:47:12 -0700

    Complete overhaul and combining of Showcatcher modules. Completely dropped all attempts to tag downloaded video files. In fact, this will work for any files that have torrent links in a <link> section of an rss feed.

--- showcatcher.py
+++ showcatcher.py
@@ -1,133 +1,255 @@
 #!/usr/bin/python
-#
-# 	Showcatcher - Takes urls from text files and downloads them using transmission
-# 	Copyright (C) 2014  Michael Hancock
-#
-# 	This program is free software: you can redistribute it and/or modify
-# 	it under the terms of the GNU General Public License as published by
-# 	the Free Software Foundation, either version 3 of the License, or
-# 	(at your option) any later version.
-#
-# 	This program is distributed in the hope that it will be useful,
-# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# 	GNU General Public License for more details.
-#
-# 	You should have received a copy of the GNU General Public License
-# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import requests
-import os
+import requests, os, feedparser, sys, subprocess, shlex, validate
 import os.path as path
-import subprocess
-import logging
-from configobj import ConfigObj
-appPath = path.dirname(os.path.abspath(__file__))
+from datetime import datetime
+from feedparser import parse
+from urllib2 import urlopen
+from urllib import urlretrieve
+from bs4 import BeautifulSoup as soup
+from configobj import ConfigObj as configobj
+
+args = sys.argv
+
+appPath = path.dirname(path.abspath(__file__))
 if appPath == '':
-	appPath = "./"
-else:
-	appPath = appPath + '/'
+	appPath = "."
+dataPath = path.join(appPath, '.showcatcher')
 	
-if path.isfile(appPath + 'config.ini') == True:
-	config = ConfigObj(appPath + 'config.ini')
-else:
-	print "Configuration file not found."
-	print "Enter absolute or relative path to your configuration file."
-	configlocation = raw_input("--> ")
-	while (path.isfile(configlocation) == False) | (configlocation.endswith('.ini') == False):
-		print "Invalid location. Try again. "
-		configlocation = raw_input("--> ")
-	config = ConfigObj(configlocation)
-	appPath = path.dirname(path.abspath(configlocation)) + '/'
+keys = {
+	'archive' : path.join(dataPath, 'archive'),
+	'cache' : path.join(dataPath, 'cache'),
+	'feeds' : path.join(dataPath, 'feeds'),
+	'log' : path.join(dataPath, 'showcatcher.log')}
 
-#This is where the files from feeder and posterget should be put
-cache = appPath + 'cache/'
-xmlcache = appPath + 'xmlcache/'
-#And this is where we will put them once they are done
-archive = appPath + 'archive/'
-logfile = appPath + 'logfile.log' 
-#api key for Pushbullet
-api = config['pushkey']
+if path.isdir(dataPath) == False:
+	os.mkdir(dataPath)
 
-#Creates a logger to keep track of things going on in logfile.log
-logging.basicConfig(format='[%(levelname)s] [%(asctime)s] %(message)s', filename=logfile, level = logging.INFO, datefmt="%m-%d-%y %H:%M:%S")
-#Allows logging to the terminal when running manually
-console = logging.StreamHandler()
-formatter = logging.Formatter(fmt = "[%(levelname)s] %(message)s")
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+if path.isdir(keys['cache']) == False:
+	os.mkdir(keys['cache'])
+	
+if path.isdir(keys['archive']) == False:
+	os.mkdir(keys['archive'])
+	
+if path.isfile(keys['feeds']) == False:
+	open(keys['feeds'], 'a').close()
+	
+class Feeder():
+	def __init__(self, keys):
+		self.archive = keys['archive']
+		self.cache = keys['cache']
+		self.feedfile = keys['feeds']
+		self.log = keys['log']
+		self.arcdict = {}
+		self.cachedict = {}
+		self.arclist = os.listdir(self.archive)
+		self.cachelist = os.listdir(self.cache)
+		self.cachelist.sort()
+		for self.i in self.arclist:
+			self.arcdict[self.i] = '1'
+		for self.i in self.cachelist:
+			self.cachedict[self.i] = '1'
+				
+	def write(self):
+		self.feeds = []
+		self.entries = []
+		self.count = {'arc' : 0, 'cache' : 0, 'write' : 0}
+		with open(self.feedfile, 'r') as self.myfile:
+			self.feeddata = self.myfile.read().split('\n')
+		for self.i in self.feeddata:
+			if (self.i != '')&(self.i.startswith('#') == False):
+				self.feeds.append(self.i)
+		if self.feeds == []:
+			self.logger("[ERROR] No feeds found in feeds file! Use '-f' or '--add-feed' options to add episode feeds")
+			return 0
+		for self.i in self.feeds:
+			self.feeddat = parse(self.i)
+			for self.i in self.feeddat.entries:
+				self.entries.append(self.i)
+		for self.i in self.entries:
+			self.title = self.i['title'].replace(' ', '.')
+			self.link = self.i['link']
+			try:
+				if self.arcdict[self.title]:
+					self.count['arc'] += 1
+			except:
+				try:
+					if self.cachedict[self.title]:
+						self.count['cache'] += 1
+				except:
+					with open(path.join(self.cache, self.title), 'w') as self.myfile:
+						self.myfile.write(self.link)
+					self.count['write'] += 1
+					self.logger('[QUEUED] ' + self.title + ' was added to queue')
+		self.total = self.count['arc'] + self.count['cache'] + self.count['write']
+		if (self.total) != 0:
+			self.logger('[QUEUE COMPLETE] New Episodes: ' + str(self.count['write']))
+			self.logger('[QUEUE COMPLETE] Already Queued: ' + str(self.count['cache']))
+			self.logger('[QUEUE COMPLETE] Already Archived: ' + str(self.count['arc']))
+		else:
+			self.logger('[ERROR] No feed information found. Something is probably wrong.')
+						
+	def move(self, title):
+		self.title = title
+		os.rename(path.join(self.cache, self.title), path.join(self.archive, self.title))
+		self.logger('[ARCHIVED] ' + self.title + ' was moved to archive.')
+		
+	def lister(self):
+		if self.cachelist != []:
+			self.cachelist.sort()
+			print 'Episodes queued for download:'
+			for self.each in self.cachelist:
+				print self.each
+		else:
+			print 'No episodes queued for download.'
+			
+	def logger(self, message):
+		self.message = message
+		print self.message
+		with open(self.log, 'a') as self.myfile:
+			self.myfile.write(str(datetime.now().strftime('[%a %m/%d/%y %H:%M:%S]')) + self.message + '\n')
 
-logging.info("Starting")
+myFeeder = Feeder(keys)
 
-if path.isdir(cache) == False:
-	logging.error("Directory '%s' does not exist" % (cache))
-	os.mkdir(cache)
-	logging.info("Created directory '%s'" % (cache))
-	
-if path.isdir(archive) == False:
-	logging.error("Directory '%s' does not exist" % (archive))
-	os.mkdir(archive)
-	logging.info("Created directory '%s'" % (archive))
-	
-if path.isdir(xmlcache) == False:
-	logging.error("Directory '%s' does not exist" % (xmlcache))
-	os.mkdir(xmlcache)
-	logging.info("Created directory '%s'" % (xmlcache))
-	
-from feeder import main as feeder
-from posterget import main as posterget
+def configreader():
+	configfile = path.join(dataPath, 'config')
+	cfg = """hostname = string(default='localhost')
+		port = string(default='9091')
+		require_auth = boolean(default=False)
+		username = string(default='')
+		password = string(default='')
+		download_directory = string(default='')"""
+	spec = cfg.split("\n")
+	config = configobj(configfile, configspec=spec)
+	validator = validate.Validator()
+	config.validate(validator, copy=True)
+	config.filename = configfile
+	config.write()
+	return config
 
-try:
-	feedmsg,arcount,cacount = feeder()
-	if arcount != 0:
-		logging.info('%s shows already in archive' % arcount)
-	if cacount != 0:
-		logging.info('%s shows ready to be downloaded' % cacount)
-	for i in feedmsg:
-		logging.info(feedmsg[i])
-except:
-	logging.error('Could not access rss feeds')
-	
-filelist = os.listdir(cache)
-	
-#Create a command to run
-#This creates a transmission-cli session to run in a screen with the id transmission
-#Will start detached
-def transmission(magnet):
-	# Executes the command in the system
-	# Requires: screen and transmission-cli
-	transmissioncommand = "transmission-remote -a %s" % (magnet)
- 	success = os.system(transmissioncommand)
-	if success == 0:
-		logging.info("Download started")
+def transmission(title, host, port, auth, authopt, downdir):
+	myFeeder.logger('[TRANSMISSION] Starting download for ' + title)
+	with open(path.join(cache, title), 'r') as myfile:
+		url = myfile.read()
+	if auth == False:
+		command = 'transmission-remote ' + host + ':' + port + ' -a "' + url + '"'
 	else:
-		logging.error("Transmission remote error")
-	return success
+		command = 'transmission-remote  ' + host + ':' + port + ' -a "' + url + '" -n ' + authopt
+	if downdir != '':
+		command = command + ' -w ' + downdir
+	transcmd = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	output,error = transcmd.communicate()
+	if error == "":
+		myFeeder.move(title)
+		myFeeder.logger('[TRANSMISSION] ' + output.strip('\n'))
+		return 0
+	else:
+		myFeeder.logger('[ERROR] ' + error.strip('\n'))
+		return 1
 
-#This will prepare a note to push to devices over Pushbullet
-def pushbullet(name):
-	body = "File %s was added to transmission." % (name)
-	payload = {'type': 'note', 'title':'Downloading new file', 'body':body}
-	note = requests.post('https://api.pushbullet.com/api/pushes', data = payload, auth = (api, ""))
-	if str(note) == "<Response [200]>":
- 		logging.info("Push message for %s sent successfully" % (name))
- 	else:
- 		logging.error("An error occured while sending push note")
-		logging.error(note)
+def addfeed(url):
+	with open(keys['feeds'], 'a') as myfile:
+		myfile.write(url + '\n')
+	myFeeder.logger('[FEEDS] Feed ' + url + ' added successfully.')
 
-if filelist != []:
-	for name in filelist:
-		location = cache + "/" + name
-		postermsg = posterget(location)
-		logging.info(postermsg)
-		with open (location, "r") as myfile:
-			data = myfile.read().replace('\n', '')
-		link,show,se,ep = data.split(' : ')
-		if transmission(link) == 0:
-			pushbullet(name)
-			os.rename(location, archive + "/" + name)
-			logging.info("Archived %s" % (name))
-else:
-	logging.info("No files to download")
-  
-logging.info("Completed")
+def printhelp():
+	options = [
+		'                                                                                ',
+		'Available options for Showcatcher:',
+		' -a   --archive                            Moves all currently queued episodes',
+		'                                           to the archive',
+		' -d   --download                           Sends all queued episodes to Trans-',
+		'                                           mission',
+		' -f   --add-feed               <url>       Appends the given URL to the list of',
+		'                                           feeds',
+		' -h   --help                               Displays this help page',
+		' -l   --list                               Lists all queued episodes',
+		' -L   --log                                Shows log from most recent full run',
+		' -q   --queue                              Checks all feeds for new episodes to',
+		'                                           add to the queue. DOES NOT SEND TO ',
+		'                                           TRANSMISSION.']
+	for each in options:
+		print each
+	
+def logreader():
+	logcmd = "sed -n '/\[SHOWCATCHER\]/=' " + keys['log']
+	logproc = subprocess.Popen(shlex.split(logcmd), stdout=subprocess.PIPE)
+	output = logproc.communicate()
+	lines = output[0].split('\n')
+	linelist = []
+	for each in lines:
+		if each != '':
+			linelist.append(int(each))
+	linelist.sort()
+	startline = lines[len(linelist)-1]
+	logproc = subprocess.Popen(shlex.split('tail -n +' + str(startline) + ' ' + keys['log']), stdout=subprocess.PIPE)
+	rawout = logproc.communicate()
+	output = rawout[0].split('\n')
+	for each in output:
+		if each != '':
+			print each
+	
+if __name__ == '__main__':
+	cache = keys['cache']
+	cachelist = os.listdir(cache)
+	cachelist.sort()
+	config = configreader()
+	host = config['hostname']
+	port = config['port']
+	auth = config['require_auth']
+	authopt = config['username'] + ':' + config['password']
+	downdir = config['download_directory']
+	try:
+		if (args[1] == '-a') | (args[1] == '--archive'):
+			myFeeder.logger('[ARCHIVE ONLY] Moving all episodes in queue to the archive')
+			if cachelist == []:
+				myFeeder.logger('[ARCHIVE COMPLETE] No episodes to archive')
+			else:
+				for each in cachelist:
+					myFeeder.move(each)
+				myFeeder.logger('[ARCHIVE COMPLETE] All episodes archived successfully')		
+		elif (args[1] == '-d') | (args[1] == '--download'):
+			myFeeder.logger('[DOWNLOAD ONLY] Starting download of already queued episodes')
+			if cachelist == []:
+				myFeeder.logger('[DOWNLOAD COMPLETE] No episodes to download')
+			else:
+				errors = 0
+				for each in cachelist:
+					test = transmission(each, host, port, auth, authopt, downdir)
+					errors += test
+				if errors > 0:
+					myFeeder.logger('[DOWNLOAD COMPLETE] There were errors adding episodes to Transmission')
+				else:
+					myFeeder.logger('[DOWNLOAD COMPLETE] Initiated all downloads successfully')
+		elif (args[1] == '-f') | (args[1] == '--add-feed'):
+			try:
+				addfeed(args[2])
+			except:
+				print "Please enter the feed url with the '" + args[1] + "' option."
+		elif (args[1] == '-h') | (args[1] == '--help'):
+			printhelp()				
+		elif (args[1] == '-l') | (args[1] == '--list'):
+			myFeeder.lister()
+		elif (args[1] == '-L') | (args[1] == '--log'):
+			logreader()
+		elif (args[1] == '-q') | (args[0] == '--queue'):
+			myFeeder.logger('[QUEUE ONLY] Checking feeds for new episodes to queue')
+			myFeeder.write()
+		else:
+			print "Invalid option '" + args[1] + "'"
+			print "Try using '-h' or '--help' for a list of valid options"
+	except:
+		myFeeder.logger('[SHOWCATCHER] Starting Showcatcher')
+		myFeeder.write()
+		cachelist = os.listdir(cache)
+		cachelist.sort()
+		if cachelist == []:
+			myFeeder.logger('[SHOWCATCHER COMPLETE] No episodes to download')
+		else:
+			errors = 0
+			for each in cachelist:
+				test = transmission(each, host, port, auth, authopt, downdir)
+				errors += test
+			if errors > 0:
+				myFeeder.logger('[SHOWCATCHER COMPLETE] There were errors adding episodes to Transmission')
+			else:
+				myFeeder.logger('[SHOWCATCHER COMPLETE] Initiated all downloads successfully')
