9d0c119a59907bd2e0f2ca395fe206eb41b161c4
archangelic <mchancock@outlook.com>
2014-09-23 15:43:52 -0400

    Added comments.

--- torrentcatcher.py
+++ torrentcatcher.py
@@ -11,7 +11,7 @@ if appPath == '':
 	appPath = "."
 dataPath = path.join(appPath, 'data')
 
-#Imports 
+#Imports modules from lib directory
 libPath = path.join(appPath, 'lib')
 modules = [path.join(libPath, 'validate'), path.join(libPath, 'argparse'), path.join(libPath, 'feedparser'), path.join(libPath, 'configobj')]
 sys.path.append(libPath)
@@ -21,11 +21,13 @@ import validate, argparse
 from feedparser import parse
 from configobj import ConfigObj as configobj
 	
+# Dictionary of values several functions use
 keys = {
 	'log' : path.join(dataPath, 'torrentcatcher.log'),
 	'config' : path.join(dataPath, 'config'),
 	'database' : path.join(dataPath, 'torcatch.db')}
 
+# Creates data directory for config file, database, and log file
 if path.isdir(dataPath) == False:
 	os.mkdir(dataPath)
 	
@@ -47,6 +49,7 @@ class Feeder():
 		self.cur.execute("SELECT * FROM Torrents WHERE downStatus=0")
 		self.cachelist = self.cur.fetchall()
 				
+	# Function to write entries from the feed to the database
 	def write(self):
 		self.entries = []
 		self.count = {'arc' : 0, 'cache' : 0, 'write' : 0}
@@ -84,13 +87,15 @@ class Feeder():
 			self.logger('[QUEUE COMPLETE] Already Archived: ' + str(self.count['arc']))
 		else:
 			self.logger('[ERROR] No feed information found. Something is probably wrong.')
-						
+					
+	# Function updates given entries to show they have been sent to the Archive
 	def move(self, title):
 		self.title = title
 		self.cur.execute("UPDATE torrents SET downStatus=1 WHERE name='%s'" % (self.title))
 		self.con.commit()
 		self.logger('[ARCHIVED] ' + self.title + ' was moved to archive.')
 		
+	# Function calls for every entry with downStatus of 0 and lists the ID # and name
 	def lister(self):
 		if self.cachelist != []:
 			print 'Torrents queued for download:'
@@ -99,6 +104,7 @@ class Feeder():
 		else:
 			print 'No torrents queued for download.'
 			
+	# Homebrewed logging solution. Any passed messages are outputted to the console as well as appended to the log
 	def logger(self, message):
 		self.message = message
 		print self.message
@@ -107,6 +113,7 @@ class Feeder():
 
 myFeeder = Feeder(keys)
 
+# Function to parse the config file and return the dictionary of values. Also creates a config file if one does not exist.
 def configreader():
 	configfile = keys['config']
 	cfg = """[transmission]
@@ -126,6 +133,7 @@ def configreader():
 	config.write()
 	return config
 
+# Function to add files to Transmission over transmission-remote
 def transmission(title, url, trconfig):
 	host = trconfig['hostname']
 	port = trconfig['port']
@@ -149,12 +157,14 @@ def transmission(title, url, trconfig):
 		myFeeder.logger('[ERROR] ' + error.strip('\n'))
 		return 1
 
+# Add Feed utility. Takes the name and URL and appends it to the config file
 def addfeed(name, url):
 	config = configreader()
 	config['feeds'][name] = url
 	config.write()
 	myFeeder.logger('[FEEDS] Feed "' + name + '" added successfully.')
 	
+# Parses out the log for the most recent run of torrentcatcher. Shows the entire log since the last time the command sans arguments ran.
 def logreader():
 	logcmd = "sed -n '/\[TORRENTCATCHER\]/=' " + keys['log']
 	logproc = subprocess.Popen(shlex.split(logcmd), stdout=subprocess.PIPE)
