cc22eacba941e0a076b1e8472a7923abe16601fc
archangelic <michaelhancock89@gmail.com>
2014-09-24 19:41:30 -0400

    Fixed vulnerability in sql queries. Wrapped database creation in 'if __name__ = ' __main__'. Cleaned up variables in Feeder.

--- torrentcatcher.py
+++ torrentcatcher.py
@@ -28,89 +28,84 @@ keys = {
 	'config' : path.join(dataPath, 'config'),
 	'database' : path.join(dataPath, 'torcatch.db')}
 
-# Creates data directory for config file, database, and log file
-if path.isdir(dataPath) == False:
-	os.mkdir(dataPath)
-	
-# Creates database if it does not exist:
-con = lite.connect(keys['database'])
-cur = con.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS torrents(id INTEGER PRIMARY KEY, name TEXT, url TEXT, source TEXT, downStatus BOOLEAN);')
-con.commit()
-	
+if __name__ == '__main__':
+	# Creates data directory for config file, database, and log file
+	if path.isdir(dataPath) == False:
+		os.mkdir(dataPath)
+	# Creates database if it does not exist:
+	con = lite.connect(keys['database'])
+	cur = con.cursor()
+	cur.execute('CREATE TABLE IF NOT EXISTS torrents(id INTEGER PRIMARY KEY, name TEXT, url TEXT, source TEXT, downStatus BOOLEAN);')
+	con.commit()
+
 class Feeder():
 	def __init__(self, keys):
 		self.configfile = keys['config']
 		self.log = keys['log']
-		self.tcdb = keys['database']
-		self.con = lite.connect(self.tcdb)
+		self.con = lite.connect(keys['database'])
 		self.cur = self.con.cursor()
-		self.cur.execute("SELECT * FROM Torrents WHERE downStatus=1")
-		self.arclist = self.cur.fetchall()
-		self.cur.execute("SELECT * FROM Torrents WHERE downStatus=0")
+		self.cur.execute("SELECT * FROM torrents WHERE downStatus=0")
 		self.cachelist = self.cur.fetchall()
 				
 	# Function to write entries from the feed to the database
 	def write(self):
-		self.entries = []
-		self.count = {'arc' : 0, 'cache' : 0, 'write' : 0}
-		self.config = configobj(self.configfile)
-		self.feeds = self.config['feeds']
-		if self.feeds == {}:
+		entries = []
+		count = {'arc' : 0, 'cache' : 0, 'write' : 0}
+		config = configobj(self.configfile)
+		feeds = config['feeds']
+		if feeds == {}:
 			self.logger("[ERROR] No feeds found in feeds file! Use '-f' or '--add-feed' options to add torrent feeds")
 			return 0
-		for self.i in self.feeds:
-			self.logger('[FEEDS] Reading entries for feed "' + self.i + '"')
-			self.feeddat = parse(self.feeds[self.i])
-			self.entries = self.feeddat.entries
-			self.feedname = self.i
-			for self.i in self.entries:
-				self.title = self.i['title']
-				self.link = self.i['link']
-				self.cur.execute("SELECT EXISTS(SELECT * FROM torrents WHERE name='%s');" % (self.title))
-				self.test = self.cur.fetchall()
-				if self.test[0][0] != 1:
-					self.cur.execute("INSERT INTO torrents(name, url, source, downStatus) VALUES ('%s', '%s', '%s', 0);" % (self.title, self.link, self.feedname))
-					self.count['write'] += 1
-					self.logger('[QUEUED] ' + self.title + ' was added to queue')
+		for i in feeds:
+			self.logger('[FEEDS] Reading entries for feed "' + i + '"')
+			feeddat = parse(feeds[i])
+			entries = feeddat.entries
+			feedname = i
+			for i in entries:
+				title = i['title']
+				link = i['link']
+				self.cur.execute("SELECT EXISTS(SELECT * FROM torrents WHERE name=?);", (title,))
+				test = self.cur.fetchall()
+				if test[0][0] != 1:
+					self.cur.execute("INSERT INTO torrents(name, url, source, downStatus) VALUES (?, ?, ?, 0);", (title, link, feedname))
+					count['write'] += 1
+					self.logger('[QUEUED] ' + title + ' was added to queue')
 				else:
-					self.cur.execute("SELECT * FROM torrents WHERE name='%s'" % (self.title))
-					self.status = self.cur.fetchall()
-					if self.status[0][4] == 1:
-						self.count['arc'] += 1
-					elif self.status[0][4] == 0:
-						self.count['cache'] += 1
+					self.cur.execute("SELECT * FROM torrents WHERE name=?", (title,))
+					status = self.cur.fetchall()
+					if status[0][4] == 1:
+						count['arc'] += 1
+					elif status[0][4] == 0:
+						count['cache'] += 1
 				self.con.commit()
-		self.total = self.count['arc'] + self.count['cache'] + self.count['write']
-		if (self.total) != 0:
-			self.logger('[QUEUE COMPLETE] New Torrents: ' + str(self.count['write']))
-			self.logger('[QUEUE COMPLETE] Already Queued: ' + str(self.count['cache']))
-			self.logger('[QUEUE COMPLETE] Already Archived: ' + str(self.count['arc']))
+		total = count['arc'] + count['cache'] + count['write']
+		if total != 0:
+			self.logger('[QUEUE COMPLETE] New Torrents: ' + str(count['write']))
+			self.logger('[QUEUE COMPLETE] Already Queued: ' + str(count['cache']))
+			self.logger('[QUEUE COMPLETE] Already Archived: ' + str(count['arc']))
 		else:
 			self.logger('[ERROR] No feed information found. Something is probably wrong.')
 					
 	# Function updates given entries to show they have been sent to the Archive
 	def move(self, title):
-		self.title = title
-		self.cur.execute("UPDATE torrents SET downStatus=1 WHERE name='%s'" % (self.title))
+		self.cur.execute("UPDATE torrents SET downStatus=1 WHERE name=?", (title,))
 		self.con.commit()
-		self.logger('[ARCHIVED] ' + self.title + ' was moved to archive.')
+		self.logger('[ARCHIVED] ' + title + ' was moved to archive.')
 		
 	# Function calls for every entry with downStatus of 0 and lists the ID # and name
 	def lister(self):
 		if self.cachelist != []:
 			print 'Torrents queued for download:'
-			for self.each in self.cachelist:
-				print '[ID %s]' % (self.each[0]), self.each[1]
+			for each in self.cachelist:
+				print '[ID %s]' % (each[0]), each[1]
 		else:
 			print 'No torrents queued for download.'
 			
 	# Homebrewed logging solution. Any passed messages are outputted to the console as well as appended to the log
 	def logger(self, message):
-		self.message = message
-		print self.message
-		with open(self.log, 'a') as self.myfile:
-			self.myfile.write(str(datetime.now().strftime('[%a %m/%d/%y %H:%M:%S]')) + self.message + '\n')
+		print message
+		with open(self.log, 'a') as myfile:
+			myfile.write(str(datetime.now().strftime('[%a %m/%d/%y %H:%M:%S]')) + message + '\n')
 
 myFeeder = Feeder(keys)
 
@@ -142,7 +137,7 @@ def transmission(title, url, trconfig):
 	authopt = trconfig['username'] + ':' + trconfig['password']
 	downdir = trconfig['download_directory']
 	myFeeder.logger('[TRANSMISSION] Starting download for ' + title)
-	if auth == False:
+	if not auth:
 		command = 'transmission-remote ' + host + ':' + port + ' -a "' + url + '"'
 	else:
 		command = 'transmission-remote  ' + host + ':' + port + ' -n ' + authopt + ' -a "' + url + '"'
@@ -191,7 +186,7 @@ def torsearch(category):
 	if category == 'id':
 		try:
 			qtest = int(query)
-			cur.execute("SELECT * FROM torrents WHERE id LIKE '{0}'".format(query))
+			cur.execute("SELECT * FROM torrents WHERE id LIKE ?", (query,))
 			results = cur.fetchall()
 			if results == []:
 				print "No results found in '{0}' for '{1}".format(category, query)
@@ -206,10 +201,11 @@ def torsearch(category):
 		except:
 			print "Please enter a valid ID number for ID searches."
 	else:
-		cur.execute("SELECT * FROM torrents WHERE {0} LIKE '%{1}%'".format(category, query))
+		lquery = '%' + query + '%'
+		cur.execute("SELECT * FROM torrents WHERE ? LIKE ?", (category, lquery))
 		results = cur.fetchall()
 		if results == []:
-			print "No results found in '{0}' for '{1}".format(category, query)
+			print "No results found in '{0}' for '{1}'".format(category, query)
 		else:
 			for each in results:
 				if each[4] == 0:
@@ -245,7 +241,7 @@ if __name__ == '__main__':
 		else:
 			for each in args.archive:
 				if each != 'all':
-					cur.execute("SELECT * FROM torrents WHERE id=%s" % (each))
+					cur.execute("SELECT * FROM torrents WHERE id=?", (each,))
 					selection = cur.fetchall()
 					seltor = selection[0]
 					if seltor[4] == 0:
@@ -273,7 +269,7 @@ if __name__ == '__main__':
 			errors = 0
 			for each in args.archive:
 				if each != 'all':
-					cur.execute("SELECT * FROM torrents WHERE id=%s" % (each))
+					cur.execute("SELECT * FROM torrents WHERE id=?", (each,))
 					selection = cur.fetchall()
 					seltor = selction[0]
 					test = transmission(seltor[1], seltor[2], trconfig)
@@ -282,20 +278,20 @@ if __name__ == '__main__':
 				myFeeder.logger('[DOWNLOAD COMPLETE] There were errors adding torrents to Transmission')
 			else:
 				myFeeder.logger('[DOWNLOAD COMPLETE] Initiated all downloads successfully')					
-	if args.add_feed == True:
+	if args.add_feed:
 		name = raw_input('Enter name for feed: ')
 		url = raw_input('Enter URL for feed: ')
 		addfeed(name, url)
-	if args.list == True:
+	if args.list:
 		myFeeder.lister()
-	if args.log == True:
+	if args.log:
 		logreader()
-	if args.queue == True:
+	if args.queue:
 		myFeeder.logger('[QUEUE ONLY] Checking feeds for new torrents to queue')
 		myFeeder.write()
 	if args.search != None:
 		torsearch(args.search[0])
-	if (args.archive==None) & (args.download==None) & (args.add_feed==False) & (args.list==False) & (args.log==False) & (args.queue==False) & (args.search==None):
+	if (args.archive==None) and (args.download==None) and (not args.add_feed) and (not args.list) and (not args.log) and (not args.queue) and (args.search==None):
 		myFeeder.logger('[TORRENTCATCHER] Starting Torrentcatcher')
 		myFeeder.write()
 		cur.execute("SELECT * FROM torrents WHERE downStatus=0")
