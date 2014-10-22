d443afade30a4ba172adc9965ba0b3ebe1984846
Michael Hancock <michaelhancock89@gmail.com>
2014-10-22 13:36:56 -0700

    Change shebang.

--- torrentcatcher.py
+++ torrentcatcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 ###########################################################################
 # torrentcatcher v1.0.1
 #     Copyright (C) 2014  Michael Hancock
