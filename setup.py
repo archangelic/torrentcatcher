5bd924c127043162c310b31800e38fd55f2c9e6d
Michael Hancock <michaelhancock89@gmail.com>
2014-12-17 17:21:42 -0500

    Changed to version 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
 	name = "torrentcatcher",
-	version = "1.1.1",
+	version = "1.2.0",
 	url = "https://github.com/archangelic/torrentcatcher",
 	description = "Takes torrent or magnet links from rss feeds you provide, parses them and sends them to transmission via the transmission-remote command line utility.",
 	dependency_links = ['http://www.voidspace.org.uk/downloads/validate.py#egg=validate-1.0.1'],
